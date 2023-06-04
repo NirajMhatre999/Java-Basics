9c69bee1ea41c7feaeffb13938a6e3a3a188ae08
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-04 16:01:56 +0530

    Add files via upload

---
+++ stringvar.java
@@ -0,0 +1,6 @@
+public class stringvar {
+    public static void main(String[] args){
+        String varname1 = "Hello", varname2 = "Hi";
+        System.out.println(varname1 + " " + varname2);
+    }
+}
