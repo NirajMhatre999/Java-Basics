c3e3445788fe9729641f98bf2043972b91449e45
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-04 16:01:30 +0530

    Add files via upload

---
+++ charvar.java
@@ -0,0 +1,6 @@
+public class charvar {
+    public static void main(String[] args){
+        char varname1 = 'i', varname2 = 65, varname3 = '9';
+        System.out.println(varname1 + "=" + varname2 + " " + varname3);
+    }
+}
