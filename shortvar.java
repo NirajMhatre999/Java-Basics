fb830ddb6fc7d82039ddb0e047fa8c774e0cf68e
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-05 23:16:30 +0530

    Add files via upload

---
+++ shortvar.java
@@ -0,0 +1,7 @@
+public class shortvar {
+    public static void main(String[] args){
+        short varname1 = 1250, varname2 = -9768;
+        System.out.println(varname1 + " " + varname2);
+    }
+}
+/* Byte accepts numbers of signed 16 bits -32768 to 32767 & is space efficient than int & long*/
