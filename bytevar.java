6179a0687865a9e970165ebd2aa521f5f72e376c
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-05 23:14:15 +0530

    Add files via upload

---
+++ bytevar.java
@@ -0,0 +1,7 @@
+public class bytevar {
+    public static void main(String[] args){
+        byte varname1 = 12, varname2 = -19;
+        System.out.println(varname1 + " " + varname2);
+    }
+}
+/* Byte accepts numbers of signed 8 bits -128 to 127 & is most space efficient*/
