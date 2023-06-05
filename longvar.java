b779123ea6b7f053eef404bdaf881af055887ea8
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-05 23:17:09 +0530

    Add files via upload

---
+++ longvar.java
@@ -0,0 +1,7 @@
+public class longvar {
+    public static void main(String[] args){
+        long varname1 = 1009528, varname2 = -1998250;
+        System.out.println(varname1 + " " + varname2);
+    }
+}
+/* Byte accepts numbers of signed 64 bits -9223372036854775808 to 9223372036854775807 & is most space consuming of all numeric variables*/
