938719c4f27cbc2c44b7bedf51400c7e603ec746
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-05 23:16:51 +0530

    Add files via upload

---
+++ intvar.java
@@ -0,0 +1,7 @@
+public class intvar {
+    public static void main(String[] args){
+        int varname1 = 64500, varname2 = -129500;
+        System.out.println(varname1 + " " + varname2);
+    }
+}
+/* Byte accepts numbers of signed 32 bits -2147483648 to 2147483647 & is space efficient than long*/
