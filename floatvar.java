f46e9787738ea505251d8af5d112f8ac3fc9bcf7
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-06 11:47:15 +0530

    Add files via upload

---
+++ floatvar.java
@@ -0,0 +1,9 @@
+public class floatvar {
+    public static void main(String[] args){
+        float varname1 = 1.98f;
+        System.out.println(varname1);
+    }
+}
+/* Byte accepts numbers of unsigned 32 bits	1.40239846e-45f to 3.40282347e+38f
+It is most space efficient method for storing decimal numbers
+Always remember to add 'f' at the end of float numbers*/
