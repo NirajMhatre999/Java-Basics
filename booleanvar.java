c696e3b7becc213bb89fbc692a6557fdfb67cac5
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-06 11:47:50 +0530

    Add files via upload

---
+++ booleanvar.java
@@ -0,0 +1,12 @@
+public class booleanvar {
+    public static void main(String[] args){
+        boolean varname1 = true;
+        System.out.println(varname1);
+        if(varname1 == true){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+    }
+}
