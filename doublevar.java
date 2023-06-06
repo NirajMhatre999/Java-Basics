06913e11f474f358f97fdda39df7ab5cf92efc6f
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-06 11:47:33 +0530

    Add files via upload

---
+++ doublevar.java
@@ -0,0 +1,9 @@
+public class doublevar {
+    public static void main(String[] args){
+        double varname1 = 1.985675;
+        System.out.println(varname1);
+    }
+}
+/* Byte accepts numbers of unsigned 64 bits	4.94065645841246544e-324 to 1.79769313486231570e+308
+It has more space complexity than float
+In double we don't need to add 'f' at the end of decimal numbers*/
