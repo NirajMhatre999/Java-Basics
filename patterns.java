7090221868ba17581a139e802db5c03089ddcc9f
NIRAJ ASHISH MHATRE <109466729+NirajMhatre999@users.noreply.github.com>
2023-06-13 15:01:07 +0530

    Add files via upload

---
+++ patterns.java
@@ -0,0 +1,12 @@
+public class patterns {
+    public static void main(String[] args){
+        System.out.println("The pattern is as follows :");
+        for(int i=0; i<=5; i++){
+            for(int j = 0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
